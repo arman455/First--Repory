ad0173c634ac3667fd94135da05f67146dcb54c8
arman455 <snositel99@gmail.com>
2022-08-08 18:55:51 +0300

    Calkulator

---
+++ 1.py
@@ -0,0 +1,8 @@
+number1 = input("Введите число:")
+nunber2 = input("Введите число:")
+def Calkulator():
+    print(number1 + nunber2)
+    print(number1 - nunber2)
+    print(number1 * nunber2)
+    print(number1 / nunber2)
+Calkulator()
